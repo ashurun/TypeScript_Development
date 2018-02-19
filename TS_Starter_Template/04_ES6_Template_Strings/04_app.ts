de6a93ee5d225536dfd07382ea43e1cae4727ad6
NAVEEN SAGGAM <thenaveensaggam@gmail.com>
2018-02-19 19:28:44 +0530

    Update 04_app.ts

--- TS_Starter_Template/04_ES6_Template_Strings/04_app.ts
+++ TS_Starter_Template/04_ES6_Template_Strings/04_app.ts
@@ -3,7 +3,8 @@
  2. Template Strings
 ---------------------------------------------------------------------------------
  Instead of using String Concatenation Operator for joining two or more
- String Literals Template Strings are best alternative. In this we use ` (back tick).
+ String Literals Template Strings are best alternative.
+ In this we use ` (back tick).
 
 */
 
@@ -17,6 +18,13 @@ let employee = {
 };
 
 let displayName =  "Meet Mr ." + employee.name;
+
+var output  = "<h1 id='display'>" + displayName + "</h1>"; // String concatenation
+
+// Template String
+output = `<h1 id="display"> ${employee.name} </h1> <span>This is some</span>`;
+
+document.getElementById('container').innerHTML = output;
 console.log(displayName);
 
 
